41fe39b8faffabad6772a8db52566acf5a08ecab
leo9154 <leo9154@gmail.com>
2017-01-12 12:09:41 +0800

    Update block_JS.js

--- block_JS.js
+++ block_JS.js
@@ -1,9 +1,28 @@
-﻿#pragma strict
+#pragma strict
+
+var lifepoint : int;
+private var numberOfHits : int;
+
 
 function Start () {
 
+	lifepoint=3;
+	numberOfHits=0;
 }
 
 function Update () {
 
 }
+
+function OnCollisionEnter2D(hit : Collision2D )
+	{
+		if (hit.gameObject.tag == "ball")
+		{
+			numberOfHits++;
+			if (numberOfHits == lifepoint) {
+				Destroy (gameObject);
+
+			}
+				
+		}
+	}
